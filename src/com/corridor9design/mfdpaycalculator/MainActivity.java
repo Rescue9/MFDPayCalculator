04020817f401c074ef99ba44112d90c244281c96
Rescue9 <Rescue9@yahoo.com>
2013-05-15 14:02:50 -0500

    hide keyboard until requested

--- src/com/corridor9design/mfdpaycalculator/MainActivity.java
+++ src/com/corridor9design/mfdpaycalculator/MainActivity.java
@@ -3,13 +3,20 @@ package com.corridor9design.mfdpaycalculator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
+	
+	// create sharedPreferences final
+	public static final String PREFS_FILE = "MFDPayCalcPrefs";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// hide the keyboard until user requests it
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	}
 
 	@Override
