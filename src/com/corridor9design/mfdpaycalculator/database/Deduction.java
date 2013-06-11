45e354ad54582ebf374c41f1523e4ab9a6e59699
Rescue9 <Rescue9@yahoo.com>
2013-06-11 15:34:03 -0500

    started implementing deduction code

--- src/com/corridor9design/mfdpaycalculator/database/Deduction.java
+++ src/com/corridor9design/mfdpaycalculator/database/Deduction.java
@@ -6,15 +6,25 @@ public class Deduction {
 	private int _id;
 	private String _deduction_name;
 	private String _deduction_amount;
-	private int _first_payday; // sqlite uses 0 (false) & 1 (true) instead of booleans
-	private int _second_payday;
-	private int _third_payday;
+	private String _first_payday;
+	private String _second_payday;
+	private String _third_payday;
 
 	public Deduction() {
 
 	}
 
-	public Deduction(String name, String amount, int first, int second, int third) {
+	public Deduction(int id, String name, String amount, String first, String second, String third) {
+		super();
+		this._id = id;
+		this._deduction_name = name;
+		this._deduction_amount = amount;
+		this._first_payday = first;
+		this._second_payday = second;
+		this._third_payday = third;
+	}
+	
+	public Deduction(String name, String amount, String first, String second, String third) {
 		super();
 		this._deduction_name = name;
 		this._deduction_amount = amount;
@@ -53,27 +63,27 @@ public class Deduction {
 		this._deduction_amount = _deduction_amount;
 	}
 
-	public int get_first_payday() {
+	public String get_first_payday() {
 		return _first_payday;
 	}
 
-	public void set_first_payday(int _first_payday) {
+	public void set_first_payday(String _first_payday) {
 		this._first_payday = _first_payday;
 	}
 
-	public int get_second_payday() {
+	public String get_second_payday() {
 		return _second_payday;
 	}
 
-	public void set_second_payday(int _second_payday) {
+	public void set_second_payday(String _second_payday) {
 		this._second_payday = _second_payday;
 	}
 
-	public int get_third_payday() {
+	public String get_third_payday() {
 		return _third_payday;
 	}
 
-	public void set_third_payday(int _third_payday) {
+	public void set_third_payday(String _third_payday) {
 		this._third_payday = _third_payday;
 	}
 
---
+++ src/com/corridor9design/mfdpaycalculator/database/MyDeductionDbHelper.java
@@ -0,0 +1,164 @@
+package com.corridor9design.mfdpaycalculator.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDeductionDbHelper extends SQLiteOpenHelper {
+	
+	// static variables
+	private static final int DATABASE_VERSION = 1; // database version
+	private static final String DATABASE_NAME = "Deductions.db"; // database name
+	private static final String TABLE_DEDUCTIONS = "deductions"; // table name
+	
+	// deductions table column names
+	private static final String KEY_ID = "_id";
+	private static final String KEY_NAME = "name";
+	private static final String KEY_AMOUNT = "amount";
+	private static final String KEY_PAYDAY1 = "payday1";
+	private static final String KEY_PAYDAY2 = "payday2";
+	private static final String KEY_PAYDAY3 = "payday3";
+	
+	// allColumns string array
+	private String allColumns[] = {
+			KEY_ID,
+			KEY_NAME,
+			KEY_AMOUNT,
+			KEY_PAYDAY1,
+			KEY_PAYDAY2,
+			KEY_PAYDAY3
+	};
+	
+	// create deduction table
+	private static final String CREATE_DEDUCTIONS_TABLE = 
+			"CREATE TABLE " + TABLE_DEDUCTIONS + " (" +
+			KEY_ID + " INTEGER PRIMARY KEY," +
+			KEY_NAME + " TEXT, " +
+			KEY_AMOUNT + " TEXT, " +
+			KEY_PAYDAY1 + " TEXT, " +
+			KEY_PAYDAY2 + " TEXT, " +
+			KEY_PAYDAY3 + " TEXT, " + ")";
+	
+	// constructor
+	public MyDeductionDbHelper(Context context){
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	// create table
+	@Override
+	public void onCreate(SQLiteDatabase db){
+		db.execSQL(CREATE_DEDUCTIONS_TABLE);
+	}
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// FIXME FUTURE: implement a way to upgrade tables without dropping them
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_DEDUCTIONS);
+		
+		// create table again
+		onCreate(db);
+	}
+	
+	// add a new deduction
+	public void addDeduction(Deduction deduction){
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(KEY_NAME, deduction.get_deduction_name());
+		values.put(KEY_AMOUNT, deduction.get_deduction_amount());
+		values.put(KEY_PAYDAY1, deduction.get_first_payday());
+		values.put(KEY_PAYDAY2, deduction.get_second_payday());
+		values.put(KEY_PAYDAY3, deduction.get_third_payday());
+		
+		// insert row
+		db.insert(TABLE_DEDUCTIONS, null, values);
+		db.close();
+	}
+	
+	// get a single deduction
+	public Deduction getDeduction(int id){
+		SQLiteDatabase db = this.getReadableDatabase();
+		
+		Cursor cursor  = db.query(TABLE_DEDUCTIONS, new String[] {
+				KEY_ID, KEY_NAME, KEY_AMOUNT, KEY_PAYDAY1, KEY_PAYDAY2, KEY_PAYDAY3 },
+				KEY_ID + "=?", new String[] {String.valueOf(id)}, null, null, null, null);
+		
+		if(cursor != null)
+			cursor.moveToFirst();
+		
+		Deduction deduction = new Deduction(Integer.parseInt(cursor.getString(0)), cursor.getString(1),
+				cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5));
+		
+		// return deduction
+		return deduction;
+	}
+	
+	// get all deductions
+	public List<Deduction> getAllDeductions(){
+		List<Deduction> deductionList = new ArrayList<Deduction>();
+		
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		Cursor cursor = db.query(TABLE_DEDUCTIONS, allColumns, null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()){
+			Deduction deduction = deductionFromCursor(cursor);
+			deductionList.add(deduction);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return deductionList;
+	}
+	
+	// get count of deductions
+	public int getDeductionCount(){
+		String countQuery = "SELECT * FROM " + TABLE_DEDUCTIONS;
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.rawQuery(countQuery, null);
+		cursor.close();
+		
+		// return count
+		return cursor.getCount();
+	}
+	
+	// update single record
+	public int updateDeduction(Deduction deduction){
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(KEY_NAME, deduction.get_deduction_name());
+		values.put(KEY_AMOUNT, deduction.get_deduction_amount());
+		values.put(KEY_PAYDAY1, deduction.get_first_payday());
+		values.put(KEY_PAYDAY2, deduction.get_second_payday());
+		values.put(KEY_PAYDAY3, deduction.get_third_payday());
+		
+		// update the row
+		return db.update(TABLE_DEDUCTIONS, values, KEY_ID + " =?", new String[] {String.valueOf(deduction.get_id()) });
+	}
+	
+	// delete a single deduction
+	public void deleteDeduction(Deduction deduction){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(TABLE_DEDUCTIONS, KEY_ID + " =?", new String[] {String.valueOf(deduction.get_id()) });
+		db.close();
+	}
+	
+	// build deduction from cursor
+	private Deduction deductionFromCursor(Cursor cursor){
+		Deduction deduction = new Deduction();
+		deduction.set_id(Integer.parseInt(cursor.getString(0)));
+		deduction.set_deduction_name(cursor.getString(1));
+		deduction.set_deduction_amount(cursor.getString(2));
+		deduction.set_first_payday(cursor.getString(3));
+		deduction.set_second_payday(cursor.getString(4));
+		deduction.set_third_payday(cursor.getString(5));
+		
+		return deduction;
+	}
+}
