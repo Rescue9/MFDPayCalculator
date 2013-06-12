a368d3466513361602d7464a6d097e97e7c20242
Rescue9 <Rescue9@yahoo.com>
2013-06-12 14:55:12 -0500

    changed deduction specific layout name

---
+++ src/com/corridor9design/mfdpaycalculator/DeductionListActivity.java
@@ -0,0 +1,14 @@
+package com.corridor9design.mfdpaycalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeductionListActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+}
