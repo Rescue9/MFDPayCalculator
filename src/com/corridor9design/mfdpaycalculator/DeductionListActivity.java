21abb2b47b181e723964f8ce8339389f13c51c6f
Rescue9 <Rescue9@yahoo.com>
2013-06-12 22:38:50 -0500

    deduction listing populating correctly

--- src/com/corridor9design/mfdpaycalculator/DeductionListActivity.java
+++ src/com/corridor9design/mfdpaycalculator/DeductionListActivity.java
@@ -3,6 +3,7 @@ package com.corridor9design.mfdpaycalculator;
 import com.corridor9design.mfdpaycalculator.database.MyDeductionDbHelper;
 
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -17,10 +18,16 @@ public class DeductionListActivity extends ListActivity {
 		
 		String[] columns = new String[] { "name", "amount" };
 		int[] to = new int[] { R.id.deduction_listing_deduction_name, R.id.deduction_listing_deduction_amount };
+		
+		Cursor cursor = db.getDeductionList();
 
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.deduction_listings,
-				db.getDeductionList(), columns, to, 0);
+				cursor, columns, to, 0);
 
+		//SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+        //        android.R.layout.simple_list_item_2, cursor,
+        //        columns,
+        //        new int[] { android.R.id.text1, android.R.id.text2 }, 0);
 		ListView listview = (ListView) findViewById(android.R.id.list);
 		listview.setAdapter(adapter);
 		
