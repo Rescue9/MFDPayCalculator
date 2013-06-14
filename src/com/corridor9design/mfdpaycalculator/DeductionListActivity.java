ac5402a7699618f0feb8ea7ea8cc3a7ac8e4f608
Rescue9 <Rescue9@yahoo.com>
2013-06-14 14:07:10 -0500

    content provider implemented. database entry and list working properly.

--- src/com/corridor9design/mfdpaycalculator/DeductionListActivity.java
+++ src/com/corridor9design/mfdpaycalculator/DeductionListActivity.java
@@ -1,32 +1,82 @@
 package com.corridor9design.mfdpaycalculator;
 
+import com.corridor9design.mfdpaycalculator.database.DeductionContentProvider;
 import com.corridor9design.mfdpaycalculator.database.MyDeductionDbHelper;
 
 import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class DeductionListActivity extends ListActivity {
+public class DeductionListActivity extends ListActivity implements LoaderCallbacks<Cursor> {
+
+	// set the projection as static
+	private static final String[] PROJECTION = new String[] { "_id", "name", "amount" };
+
+	// set the loader's unique id. loader id's are specific to the Activity or Fragment in which they reside
+	private static final int LOADER_ID = 1;
+
+	// the callback through which we will interact with the LoaderManager
+	private LoaderCallbacks<Cursor> mCallbacks;
+
+	// the adapter that binds data to our listview
+	private SimpleCursorAdapter madapter;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_deduction_list);
 
-		MyDeductionDbHelper db = new MyDeductionDbHelper(this);
-		
-		String[] columns = new String[] { "name", "amount" };
-		int[] to = new int[] { R.id.deduction_listing_deduction_name, R.id.deduction_listing_deduction_amount };
-		
-		Cursor cursor = db.getDeductionList();
+		String[] DataColumns = new String[] { "name", "amount" };
+		int[] viewIDs = new int[] { R.id.deduction_listing_deduction_name, R.id.deduction_listing_deduction_amount };
 
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.deduction_listings,
-				cursor, columns, to, 0);
+		madapter = new SimpleCursorAdapter(this, R.layout.deduction_listings, null, DataColumns, viewIDs, 0);
 
 		ListView listview = (ListView) findViewById(android.R.id.list);
-		listview.setAdapter(adapter);
-		
+		listview.setAdapter(madapter);
+
+		// The Activity (which implements the LoaderCallbacks<Cursor> interface) is the callbacks object through which
+		// we will interact with the LoaderManager. The LoaderManager uses this object to instantiate the Loader and to
+		// notify the client when data is made available/unavailable.
+		mCallbacks = this;
+
+		// Initialize the Loader with id '1' and callbacks 'mCallbacks'. If the loader doesn't already exist, one is
+		// created. Otherwise, the already created Loader is reused. In either case, the LoaderManager will manage the
+		// Loader across the Activity/Fragment lifecycle, will receive any new loads once they have completed, and will
+		// report this new data back to the 'mCallbacks' object.
+		LoaderManager lm = getLoaderManager();
+		lm.initLoader(LOADER_ID, null, mCallbacks);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// create a new CursorLoader with the following query parameters
+		return new CursorLoader(DeductionListActivity.this, DeductionContentProvider.CONTENT_URI, PROJECTION, null,
+				null, null);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		// a switch case is useful when dealing with multiple loaders/ids
+		switch (loader.getId()) {
+		case LOADER_ID:
+			// the asyncronous load is complete and the data is now available for use. Only now can we associate the
+			// query Cursor with the SimpleCursoeAdapter
+			madapter.swapCursor(cursor);
+			break;
+		}
+		// the listview now displays the queried data
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// for whatever reason, the loader's data is now unavailable. remove any references to the old data by replacing
+		// it with a null Cursor
+		madapter.swapCursor(null);
 	}
 
 }
--- src/com/corridor9design/mfdpaycalculator/database/DeductionContentProvider.java
+++ src/com/corridor9design/mfdpaycalculator/database/DeductionContentProvider.java
@@ -14,17 +14,17 @@ import android.net.Uri;
 import android.text.TextUtils;
 
 public class DeductionContentProvider extends ContentProvider {
-	
+
 	// declare database
 	private MyDeductionDbHelper database;
-	
+
 	// public constants for client
-	private static final String AUTHORITY = "com.corridor9design.mfdpaycalculator.deduction.contentprovider";
+	private static final String AUTHORITY = "com.corridor9design.mfdpaycalculator.database.deductioncontentprovider";
 	private static final String BASE_PATH = "deduction";
 	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
 	public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/deduction";
 	public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/deduction";
-	
+
 	// create UriMatcher and add Uris
 	private static final int DEDUCTION_LIST = 1;
 	private static final int DEDUCTION_ID = 2;
@@ -33,25 +33,25 @@ public class DeductionContentProvider extends ContentProvider {
 		sURIMatcher.addURI(AUTHORITY, BASE_PATH, DEDUCTION_LIST);
 		sURIMatcher.addURI(AUTHORITY, BASE_PATH + "/#", DEDUCTION_ID);
 	}
-	
-	public boolean onCreate(){
+
+	public boolean onCreate() {
 		database = new MyDeductionDbHelper(getContext());
 		return false;
 	}
-	
-	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder){
+
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		// using SQLiteQueryBuilder instead of query
 		SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
-		
-		//check to make sure columns in projection exist in database
+
+		// check to make sure columns in projection exist in database
 		checkColumns(projection);
-		
+
 		// set the table
 		queryBuilder.setTables(Deduction.TABLE_DEDUCTION);
-		
+
 		// create a switch statement based upon directory or item uris
 		int uriType = sURIMatcher.match(uri);
-		switch(uriType){
+		switch (uriType) {
 		case DEDUCTION_LIST:
 			break;
 		case DEDUCTION_ID:
@@ -61,24 +61,24 @@ public class DeductionContentProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI: " + uri);
 		}
-		
+
 		SQLiteDatabase db = database.getWritableDatabase();
 		Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
-		
+
 		return cursor;
 	}
-	
+
 	@Override
-	public String getType(Uri uri){
+	public String getType(Uri uri) {
 		return null;
 	}
-	
+
 	@Override
-	public Uri insert(Uri uri, ContentValues values){
+	public Uri insert(Uri uri, ContentValues values) {
 		int uriType = sURIMatcher.match(uri);
 		SQLiteDatabase db = database.getWritableDatabase();
 		long id = 0;
-		switch (uriType){
+		switch (uriType) {
 		case DEDUCTION_LIST:
 			id = db.insert(Deduction.TABLE_DEDUCTION, null, values);
 			break;
@@ -86,24 +86,24 @@ public class DeductionContentProvider extends ContentProvider {
 			// something went wrong
 			throw new IllegalArgumentException("Unknown URI: " + uri);
 		}
-		
+
 		// notify listeners of change and return item's uri
 		getContext().getContentResolver().notifyChange(uri, null);
 		return Uri.parse(BASE_PATH + "/" + id);
 	}
-	
+
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		int uriType = sURIMatcher.match(uri);
 		SQLiteDatabase db = database.getWritableDatabase();
 		int rowsDeleted = 0;
-		switch (uriType){
+		switch (uriType) {
 		case DEDUCTION_LIST:
 			rowsDeleted = db.delete(Deduction.TABLE_DEDUCTION, selection, selectionArgs);
 			break;
 		case DEDUCTION_ID:
 			String id = uri.getLastPathSegment();
-			if (TextUtils.isEmpty(selection)){
+			if (TextUtils.isEmpty(selection)) {
 				rowsDeleted = db.delete(Deduction.TABLE_DEDUCTION, Deduction.TABLE_DEDUCTION + "=" + id, null);
 			} else {
 				rowsDeleted = db.delete(Deduction.TABLE_DEDUCTION, Deduction.TABLE_DEDUCTION + "=" + id, selectionArgs);
@@ -112,52 +112,53 @@ public class DeductionContentProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI: " + uri);
 		}
-		
+
 		// notify listeners of change and return item's uri
 		getContext().getContentResolver().notifyChange(uri, null);
 		return rowsDeleted;
 	}
-	
+
 	@Override
-	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs){
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
 		int uriType = sURIMatcher.match(uri);
 		SQLiteDatabase db = database.getWritableDatabase();
 		int rowsUpdated = 0;
-		switch (uriType){
+		switch (uriType) {
 		case DEDUCTION_LIST:
 			rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, selection, selectionArgs);
 			break;
 		case DEDUCTION_ID:
 			String id = uri.getLastPathSegment();
-			if (TextUtils.isEmpty(selection)){
-				rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, Deduction.TABLE_DEDUCTION + "=" + id + " and " + selection, null);
+			if (TextUtils.isEmpty(selection)) {
+				rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, Deduction.TABLE_DEDUCTION + "=" + id
+						+ " and " + selection, null);
 			} else {
-			rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, Deduction.TABLE_DEDUCTION + "=" + id + " and " + selection, selectionArgs);
+				rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, Deduction.TABLE_DEDUCTION + "=" + id
+						+ " and " + selection, selectionArgs);
 			}
 			break;
 		default:
 			throw new IllegalArgumentException("Unknown URI: " + uri);
 		}
-		
+
 		// notify listeners of change and return item's uri
 		getContext().getContentResolver().notifyChange(uri, null);
 		return rowsUpdated;
 	}
-	
-	private void checkColumns(String[] projection){
+
+	private void checkColumns(String[] projection) {
 		String[] available = { Deduction.COLUMN_AMOUNT, Deduction.COLUMN_DESCRIPTION, Deduction.COLUMN_ID,
 				Deduction.COLUMN_NAME, Deduction.COLUMN_PAYDAY1, Deduction.COLUMN_PAYDAY2, Deduction.COLUMN_PAYDAY3 };
-		
-		if (projection != null){
+
+		if (projection != null) {
 			HashSet<String> requestedColumns = new HashSet<String>(Arrays.asList(projection));
 			HashSet<String> availableColumns = new HashSet<String>(Arrays.asList(available));
-			
+
 			// check to see if the columns requested are available
-			if (!availableColumns.containsAll(requestedColumns)){
+			if (!availableColumns.containsAll(requestedColumns)) {
 				throw new IllegalArgumentException("Unknown columns in projection");
 			}
 		}
 	}
-
 
 }
