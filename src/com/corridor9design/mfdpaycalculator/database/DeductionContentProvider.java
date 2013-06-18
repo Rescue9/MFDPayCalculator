a705111c59efa06ed876a87e444948aede960f3c
Rescue9 <Rescue9@yahoo.com>
2013-06-17 23:10:44 -0500

    finished refactoring com.corridor9design.mfdpaycalculator.database

--- src/com/corridor9design/mfdpaycalculator/database/DeductionContentProvider.java
+++ src/com/corridor9design/mfdpaycalculator/database/DeductionContentProvider.java
@@ -1,3 +1,11 @@
+/**
+ * Program: DeductionContentProvider.java
+ * Programmer: Andrew Buskov
+ * Date: Jun 17, 2013
+ * Purpose: To create a content provider that can be used to get information
+ *  from the database throughout the application and from other applications.
+ */
+
 package com.corridor9design.mfdpaycalculator.database;
 
 import java.util.Arrays;
@@ -18,7 +26,7 @@ public class DeductionContentProvider extends ContentProvider {
 	// declare database
 	private MyDeductionDbHelper database;
 
-	// public constants for client
+	// create constants that can be used by the client
 	private static final String AUTHORITY = "com.corridor9design.mfdpaycalculator.database.deductioncontentprovider";
 	private static final String BASE_PATH = "deduction";
 	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
@@ -39,6 +47,7 @@ public class DeductionContentProvider extends ContentProvider {
 		return false;
 	}
 
+	// create a cursor query that will return information to the content resolver
 	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		// using SQLiteQueryBuilder instead of query
 		SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
@@ -46,7 +55,7 @@ public class DeductionContentProvider extends ContentProvider {
 		// check to make sure columns in projection exist in database
 		checkColumns(projection);
 
-		// set the table
+		// tell the builder what table we want checked
 		queryBuilder.setTables(Deduction.TABLE_DEDUCTION);
 
 		// create a switch statement based upon directory or item uris
@@ -62,9 +71,11 @@ public class DeductionContentProvider extends ContentProvider {
 			throw new IllegalArgumentException("Unknown URI: " + uri);
 		}
 
+		// get the database object, query the database, return results to a cursor
 		SQLiteDatabase db = database.getWritableDatabase();
 		Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 
+		// set notification uri for change
 		cursor.setNotificationUri(getContext().getContentResolver(), uri);
 
 		return cursor;
@@ -75,11 +86,15 @@ public class DeductionContentProvider extends ContentProvider {
 		return null;
 	}
 
+	// used for inserting data into the database
 	@Override
 	public Uri insert(Uri uri, ContentValues values) {
+		// find out if it's a dir or id uri
 		int uriType = sURIMatcher.match(uri);
 		SQLiteDatabase db = database.getWritableDatabase();
 		long id = 0;
+
+		// if it is not a dir (list) uri type fail. insert cannot be done on id uri's
 		switch (uriType) {
 		case DEDUCTION_LIST:
 			id = db.insert(Deduction.TABLE_DEDUCTION, null, values);
@@ -96,17 +111,25 @@ public class DeductionContentProvider extends ContentProvider {
 
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// find out if it's a dir or id uri
 		int uriType = sURIMatcher.match(uri);
 		SQLiteDatabase db = database.getWritableDatabase();
 		int rowsDeleted = 0;
+
+		// if it's a dir type, simply pass the selection & arguments
 		switch (uriType) {
 		case DEDUCTION_LIST:
 			rowsDeleted = db.delete(Deduction.TABLE_DEDUCTION, selection, selectionArgs);
 			break;
+
+		// if it's an id, get the id from the uri...
 		case DEDUCTION_ID:
 			String id = uri.getLastPathSegment();
+			// if the selection argument is empty, just delete the row
 			if (TextUtils.isEmpty(selection)) {
 				rowsDeleted = db.delete(Deduction.TABLE_DEDUCTION, Deduction.TABLE_DEDUCTION + "=" + id, null);
+
+				// if the selection argument has value, pass that to delete
 			} else {
 				rowsDeleted = db.delete(Deduction.TABLE_DEDUCTION, Deduction.TABLE_DEDUCTION + "=" + id, selectionArgs);
 			}
@@ -122,18 +145,24 @@ public class DeductionContentProvider extends ContentProvider {
 
 	@Override
 	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		// find out if it's a dir or id uri
 		int uriType = sURIMatcher.match(uri);
 		SQLiteDatabase db = database.getWritableDatabase();
 		int rowsUpdated = 0;
+
+		// if it's a dir type, simply pass the selection & arguments
 		switch (uriType) {
 		case DEDUCTION_LIST:
 			rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, selection, selectionArgs);
 			break;
 		case DEDUCTION_ID:
 			String id = uri.getLastPathSegment();
+			// if the selection argument is empty, just delete the row
 			if (TextUtils.isEmpty(selection)) {
 				rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, Deduction.TABLE_DEDUCTION + "=" + id
 						+ " and " + selection, null);
+
+				// if the selection argument has value, pass that to delete
 			} else {
 				rowsUpdated = db.update(Deduction.TABLE_DEDUCTION, values, Deduction.TABLE_DEDUCTION + "=" + id
 						+ " and " + selection, selectionArgs);
@@ -148,6 +177,7 @@ public class DeductionContentProvider extends ContentProvider {
 		return rowsUpdated;
 	}
 
+	// check the columns received from the calling method to make sure they exist in our table
 	private void checkColumns(String[] projection) {
 		String[] available = { Deduction.COLUMN_AMOUNT, Deduction.COLUMN_DESCRIPTION, Deduction.COLUMN_ID,
 				Deduction.COLUMN_NAME, Deduction.COLUMN_PAYDAY1, Deduction.COLUMN_PAYDAY2, Deduction.COLUMN_PAYDAY3 };
@@ -162,5 +192,4 @@ public class DeductionContentProvider extends ContentProvider {
 			}
 		}
 	}
-
 }
